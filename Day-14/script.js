291e4cf15716d0d5a8092540893efab3c13d28bf
shubhammt123 <shubhammt123@gmail.com>
2025-09-05 11:53:02 +0530

    Array methods

--- Day-14/script.js
+++ Day-14/script.js
@@ -1,6 +1,6 @@
 // array methods
 
-// push , pop , shift , unshift , map , filter , reduce  , splice , split , indexOf , lastIndexOf , concat , includes , find , findIndex , some , every ,  forEach , sort , reverse , flat  , join ,  
+// push , pop , shift , unshift , map , filter , reduce  , splice , slice , indexOf , lastIndexOf , concat , includes , find , findIndex , some , every ,  forEach , sort , reverse , flat  , join
 
 // let arr = [10,20,30,40,50,60];
 
@@ -61,22 +61,128 @@
 // calculator
 
 
-function calculator(a,b,operation){
-    console.log(operation)
-    operation(a,b);
-}
+// function calculator(a,b,operation){
+//     console.log(operation)
+//     operation(a,b);
+// }
 
-calculator(100,200,(x,y)=>{
-    console.log(x/y);
-});
+// calculator(100,200,(x,y)=>{
+//     console.log(x/y);
+// });
 
-calculator(100,200,(x,y)=>{
-    console.log(x*y);
-});
+// calculator(100,200,(x,y)=>{
+//     console.log(x*y);
+// });
 
-calculator(100,200,(x,y)=>{
-    console.log(x+y);
-})
-calculator(100,200,(x,y)=>{
-    console.log(x-y);
-})
+// calculator(100,200,(x,y)=>{
+//     console.log(x+y);
+// })
+// calculator(100,200,(x,y)=>{
+//     console.log(x-y);
+// })
+
+// let arr = [10,20,30,40,50,60];
+
+// let result = arr.map((item)=>{
+//     return item**2;
+// });
+
+// console.log(result);
+
+// let products = [
+//     {
+//         productName : "Iphone",
+//         productPrice : "$999",
+//         category : "smartphone",
+//         description : "This is 16 series of iphone"
+//     },
+//     {
+//         productName : "Samsung",
+//         productPrice : "$999",
+//         category : "smartphone",
+//         description : "This is S25 series of Samsung"
+//     },
+    
+//     {
+//         productName : "Google Pixel 9",
+//         productPrice : "$999",
+//         category : "smartphone",
+//         description : "This is pixel series of google"
+//     },
+    
+//     {
+//         productName : "Nothing",
+//         productPrice : "$999",
+//         category : "smartphone",
+//         description : "This is Nothing phone"
+//     },
+// ]
+
+// let results = products.map((product,index)=>{
+//     let newObj = {...product , id : index+1};
+//     return newObj;
+// })
+
+// map does't change original array and returns the new array with the same length of the original array
+
+// console.log(results);
+// console.log("---------****----------")
+// console.log(products); 
+
+// let arr = [10,20,30,40,50,60];
+
+// let result = arr.map((item)=>{
+//     if(item>30){
+//         return item
+//     };
+// });
+
+// console.log(result);
+
+// filter
+
+// let arr = [10,20,30,40,50,60];
+
+// let result = arr.filter((item)=>{
+//     return item>30;
+// });
+
+// console.log(result);
+// console.log(arr);
+
+
+// reduce
+// let arr = [10,20,30,40,50,60];
+// let result = arr.reduce((acc,curr)=>{
+//     return acc+curr
+// },1000);
+
+// console.log(result)
+
+// split , splice
+
+// let arr = [10,20,30,"shubham",40,50,"shubham",60,"shubham",70];
+
+// let result = arr.slice(2);
+// console.log(result);
+
+// let result = arr.splice(3,0,"Hello","world");
+
+// console.log(arr)
+
+// let result = arr.indexOf("jain");
+// console.log(result);
+
+// concat , includes
+
+// let arr1 = [10,20,30];
+// let arr2 = [40,50,60];
+
+// let result = arr1.concat("shubham",arr2);
+
+// console.log(result)
+
+let arr = [10,20,30,"shubham",40,50,"shubham",60,"shubham",70];
+
+let result = arr.includes(50);
+console.log(result)
