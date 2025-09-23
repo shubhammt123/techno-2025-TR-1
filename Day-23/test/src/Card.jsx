ca12aa2454ed91ecdf4975de05b3ad9b5473236c
shubhammt123 <shubhammt123@gmail.com>
2025-09-23 11:23:29 +0530

    Conditional rendering

--- Day-23/test/src/Card.jsx
+++ Day-23/test/src/Card.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Card = ({title , description , image}) => {
-  
+const Card = ({title , description , image  , setOpen , id , setId}) => {
+
   return (
-    <div className='card'>
+    <div className='card' onClick={()=>{setOpen(true); setId(id)}}>
         <img src={image} alt=""   className='image' />
         <h1 className='title'>{title}</h1>
         <p className='para'>{description}</p>
--- Day-23/test/src/Effect.jsx
+++ Day-23/test/src/Effect.jsx
@@ -4,7 +4,9 @@ import Card from "./Card";
 const Effect = () => {
 
     const [data , setData] = useState([]);
-    const [count , setCount] = useState(1);
+    const [id , setId] = useState(null);
+    const [open , setOpen] = useState(false);
+    const [productData , setProductData] = useState({});
 
     const fetchData = async ()=>{
         try {
@@ -16,10 +18,27 @@ const Effect = () => {
             console.log(error)
         }
     }
+    const fetchDataWithId = async (productId)=>{
+        try {
+            const response = await fetch(`https://fakestoreapi.com/products/${productId}`);
+            const productData = await response.json();
+            // setData(productData);
+            setProductData(productData)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
 
     useEffect(()=>{
         fetchData();
     },[]);
+
+    useEffect(()=>{
+       if(id){
+         fetchDataWithId(id);
+       }
+    },[id])
 
     const style = {
         display : "flex",
@@ -35,11 +54,22 @@ const Effect = () => {
             {data.map((item)=>{
         return (
             
-            <Card title={item.title} description={item.description} image={item.image} />
+            <Card title={item.title} description={item.description} image={item.image} setOpen={setOpen} setId={setId} id={item.id} />
             
         )
     })}
         </div>
+
+        {open && (
+            <div className="modal">
+            <div className="modal-container">
+                <button onClick={()=>{setOpen(false);setId(null);setProductData({})}}>X</button>
+                <div>
+                    <img src={productData.image} alt="" style={{width :"25%"}} />
+                </div>
+            </div>
+            </div>
+        )}
         </div>
   )
 }
