ed28ff6e146ab5f434677ce7b305d2e17f5fedfd
shubhammt123 <shubhammt123@gmail.com>
2025-09-24 11:03:37 +0530

    login/signup completed

--- Day-23/test/src/Login.jsx
+++ Day-23/test/src/Login.jsx
@@ -2,23 +2,74 @@ import React, { useState } from 'react'
 
 const Login = () => {
 
-    const [isSignup , setIsSignup] = useState(false);
+    const [isSignup, setIsSignup] = useState(false);
+    const [formData , setFormData] = useState({
+        userName : "",
+        email : "",
+        password : "",
+        confirmPassword : "",
+        contactNo : ""
+    });
 
-    console.log(isSignup)
-  return (
-    <div className='form' style={{height : isSignup ? "85%" : "60%"}}>
-        <h1 style={{textAlign : "center" , color : "grey"}}>{isSignup ? "Welcome , Signup!" : "Welcome Back, Login!"}</h1>
-        <form style={{display : "flex" , flexDirection : "column" , justifyContent : "center" , alignItems  : "center", gap  : "20px"}}>
-            {isSignup && <input type="text" placeholder='User Name' />}
-            <input type="email" placeholder='Email' />
-            <input type="password" placeholder='Password' />
-            {isSignup && <input type='password' placeholder='Confirm Password' />}
-            {isSignup && <input type='text' placeholder='Contact No.' />}
-            <button>{isSignup ? "Signup" : "Login" }</button>
-        </form>
-        <div style={{textAlign : "center" , color : "gray"}}>{isSignup ? "Already have an account?" : "Don't have an account?"} <span style={{color : "blue" , cursor : "pointer"}} onClick={()=>{setIsSignup(!isSignup)}}>{isSignup ? "Login" : "Signup"}</span></div>
-    </div>
-  )
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        console.log(formData.confirmPassword)
+        console.log(formData.password)
+        if(formData.confirmPassword !== formData.password){
+            alert("Password and confirm password should be same");
+            return;
+        }
+        console.log(formData)
+    }
+
+    const handleChange = (e)=>{
+       setFormData({...formData , [e.target.name] : e.target.value});
+    }
+
+
+    return (
+        <div className='form' style={{ height: isSignup ? "85%" : "60%" }}>
+
+            <h1 style={{ textAlign: "center", color: "grey" }}>{isSignup ? "Welcome , Signup!" : "Welcome Back, Login!"}</h1>
+
+            <form style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", gap: "20px" }} onSubmit={handleSubmit}>
+
+                {isSignup && <input type="text" placeholder='User Name' name='userName' onChange={handleChange} value={formData.userName} />}
+
+                <input type="email" placeholder='Email' name='email' onChange={handleChange} value={formData.email} />
+
+                <input type="password" placeholder='Password' name='password' onChange={handleChange} value={formData.password} />
+
+
+                {isSignup && <input type='password' name='confirmPassword' placeholder='Confirm Password' onChange={handleChange} value={formData.confirmPassword} />}
+
+                {isSignup && <input type='text' placeholder='Contact No.' name='contactNo' onChange={handleChange} value={formData.contactNo} />}
+
+                <button>{isSignup ? "Signup" : "Login"}</button>
+
+            </form>
+
+            <div style={{ textAlign: "center", color: "gray" }}>
+                {isSignup ? "Already have an account?" : "Don't have an account?"} <span style={{ color: "blue", cursor: "pointer" }} onClick={() => { setIsSignup(!isSignup);
+                if(!isSignup){
+                    setFormData({
+                        userName : "",
+                        email : "",
+                        password : "",
+                        confirmPassword : "",
+                        contactNo : ""
+                    });
+                }else{
+                    setFormData({
+                        email : "",
+                        password : "",
+                    });
+                }
+                
+                 }}>{isSignup ? "Login" : "Signup"}</span>
+            </div>
+        </div>
+    )
 }
 
 export default Login
