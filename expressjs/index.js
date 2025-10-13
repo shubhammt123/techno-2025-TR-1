6085ef12c7ff3fa52450355ce7fe7e2a6fbfe1df
shubhammt123 <shubhammt123@gmail.com>
2025-10-13 10:38:24 +0530

    get product by  id

--- expressjs/index.js
+++ expressjs/index.js
@@ -20,17 +20,29 @@ app.get("/getAllProduct",(req,res)=>{
     let productData = fs.readFileSync("product.json","utf-8");
     res.send(productData);
 });
-app.get("/getProductById",()=>{});
-app.post("/createProduct",(req,res)=>{
-    const reqBody = req.body;
-    const productData = fs.readFileSync("product.json");
-    const parsedData = JSON.parse(productData);
-    parsedData.push(reqBody);
-    fs.writeFileSync("product.json",JSON.stringify(parsedData , null ,2));
-    res.status(201).send({message : "Product Created" , product : reqBody})
+app.get("/getProductById/:productId",(req,res)=>{
+    const {productId} = req.params;
+    console.log(typeof productId)
+    const productData = JSON.parse(fs.readFileSync("product.json","utf-8"));
+    const filteredData = productData.filter((item)=>item.id===+productId);
+    if(filteredData.length === 0) return res.status(404).send({message : "Product not found"});
+    res.status(200).send({message : "Product Fetched" , product : filteredData[0]});
 
 });
-app.put("/updateProduct",()=>{});
+app.post("/createProduct",(req,res)=>{
+    const reqBody = req.body;
+    console.log(reqBody)
+    const productData = fs.readFileSync("product.json","utf-8");
+    console.log(productData)
+    const parsedData = JSON.parse(productData);
+    const updatedReqBody = {...reqBody , id : Date.now()};
+    parsedData.push(updatedReqBody);
+    fs.writeFileSync("product.json",JSON.stringify(parsedData , null ,2));
+    res.status(201).send({message : "Product Created" , product : updatedReqBody})
+});
+app.put("/updateProduct",(req,res)=>{
+
+});
 app.delete("/deleteProduct",()=>{});
 
 
